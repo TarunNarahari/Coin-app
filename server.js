c7976071cb78e64aed7e4b22b05c48d8b6d6b6c2
Tarun Narahari <tarun.narahari@gmail.com>
2022-11-16 23:06:40 -0500

    changing ports

--- server.js
+++ server.js
@@ -39,8 +39,10 @@ User.create(user, (err, data) => {
 
 
 // create a server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}.`);
+});
 
 
 
